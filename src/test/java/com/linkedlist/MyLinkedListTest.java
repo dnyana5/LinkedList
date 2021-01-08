d62148ffefa3836656206fc995511106ae11f483
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-01-08 16:37:55 +0530

    Added:Appended numbers to Linked List

--- src/test/java/com/linkedlist/MyLinkedListTest.java
+++ src/test/java/com/linkedlist/MyLinkedListTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class MyLinkedListTest {
     @Test
-    public void given3NumberWhenLinkedListShouldBeAddedToTop() {
+    public void given3NumberWhenAddedToLinkedListShouldBeAddedToTop() {
         MyNode<Integer> myFirstNode = new MyNode<Integer>(70);
         MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
         MyNode<Integer> myThirdNode = new MyNode<Integer>(56);
@@ -19,5 +19,22 @@ public class MyLinkedListTest {
                 myLinkdList.tail.equals(myFirstNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given3NumberWhenAppendedLinkedListShouldBeAddedToLast() {
+        MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+        MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+        MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+        MyLinkedList myLinkdList = new MyLinkedList();
+        myLinkdList.append(myFirstNode);
+        myLinkdList.append(mySecondNode);
+        myLinkdList.append(myThirdNode);
+        myLinkdList.printMyNodes();
+        boolean result = myLinkdList.head.equals(myFirstNode) &&
+                myLinkdList.head.getNext().equals(mySecondNode) &&
+                myLinkdList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
+
 
 }
