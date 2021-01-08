7064d96b60967af8b7a754e740245c991063ee84
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-01-08 16:11:45 +0530

    Added:Simple Linked List with 3 numbers

--- src/main/java/com/linkedlist/MyNode.java
+++ src/main/java/com/linkedlist/MyNode.java
@@ -1,7 +1,19 @@
 package com.linkedlist;
 
-public class MyNode {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Linked List problem");
+public class MyNode <K> {
+    private K key;
+    private MyNode next;
+
+    public MyNode(K key){
+        this.key = key;
+        this.next = null;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
     }
 }
